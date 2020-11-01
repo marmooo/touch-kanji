26c41a6bf2f9bfafbe0c85d1d504add614c8e66c
marmooo <marmooo@users.noreply.github.com>
2020-11-01 21:15:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# タッチ漢字ドリル
+[自動採点付きの漢字ドリル](https://marmooo.github.io/touch-kanji/)です。
+
+## Installation
+1. install [KanjiVG/kanjivg](https://github.com/KanjiVG/kanjivg)
+2. install [marmooo/ngram-idioms](https://github.com/marmooo/ngram-idioms)
+3. install [marmooo/sudachi-idioms](https://github.com/marmooo/sudachi-idioms)
+4. ```npm install```
+
+## Build
+1. ```node build.js```
+2. ```node build.sh```
+
+## References
+- [常用漢字表](https://www.bunka.go.jp/kokugo_nihongo/sisaku/joho/joho/kakuki/14/tosin02/index.html)
+
+## License
+CC BY-SA 4.0
+
+## Attribution
+- [KanjiVG/kanjivg](https://github.com/KanjiVG/kanjivg) is licensed under the [CC BY-SA 3.0](https://creativecommons.org/licenses/by-sa/3.0/).
---
+++ docs/sw.js
@@ -0,0 +1,52 @@
+var CACHE_NAME = '2020-11-01 21:20';
+var urlsToCache = [
+  '/touch-kanji/',
+  '/touch-kanji/index.js',
+  '/touch-kanji/drill.js',
+  '/touch-kanji/drill/',
+  '/touch-kanji/svg/eraser.svg',
+  '/touch-kanji/svg/dict.svg',
+  '/touch-kanji/mp3/correct1.mp3',
+  '/touch-kanji/mp3/correct3.mp3',
+  '/touch-kanji/mp3/incorrect1.mp3',
+  '/touch-kanji/mp3/stupid5.mp3',
+  'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css',
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches
+    .open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
