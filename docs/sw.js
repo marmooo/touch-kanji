f8763cfaea099f931a5df27d010cdf0bb35cdbbe
marmooo <marmooo@users.noreply.github.com>
2021-06-04 09:35:17 +0900

    update libraries

--- docs/sw.js
+++ docs/sw.js
@@ -10,7 +10,7 @@ var urlsToCache = [
   '/touch-kanji/mp3/correct3.mp3',
   '/touch-kanji/mp3/incorrect1.mp3',
   '/touch-kanji/mp3/stupid5.mp3',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css',
 ];
 
 self.addEventListener('install', function(event) {
